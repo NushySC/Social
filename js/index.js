4ee5a950ee5e357f5ba8e4ce2b5597f6b1f5a1e2
Anuska <ank.sampedro@gmail.com>
2019-07-04 18:23:18 +0200

    modal

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const modal = document.getElementById('side-panel--modal');
+const btn = document.getElementById("show-panel");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
